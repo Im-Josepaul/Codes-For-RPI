455ec33883f7e710f22463e96f9e1fd1eeb543b1
hackerfaded <fadedhacker18@gmail.com>
2021-04-27 19:14:02 +0530

    File is added

---
+++ GUI of Bulb and switch.py
@@ -0,0 +1,32 @@
+import PySimpleGUI as sg    #Importing GUI
+import webbrowser
+import RPi.GPIO as GPIO     #Importing GPIO pins of RPI
+
+GPIO.setmode(GPIO.BOARD)    #GPIO modes are set to board.
+GPIO.setwarnings(False)
+ledPin = 12
+GPIO.setup(ledPin, GPIO.OUT) #GPIO Pin 12 is setuped as output pin.
+
+layout= [
+    [sg.Text("You can light a bulb connected to your Raspberry Pi using this software.")],
+    [sg.Text("Please make sure all connections are made as shown"), sg.Text("here.", key="-HERE-",text_color="Blue", enable_events=True)],
+    [sg.Text("Press the below button to switch ON and OFF the bulb.")],
+    [sg.Button(size=(20,20), image_filename = "/home/josepaul/Test Files/On Button.png", enable_events= True, key="-ON-", pad=((170,0),(0,0))), 
+    sg.Button(size=(20,20), image_filename = "/home/josepaul/Test Files/Off Button.png", enable_events= True, key="-OFF-", pad=((20,0),(0,0)))],
+    [sg.Button("Exit")]
+    ]
+window = sg.Window("Light a bulb", layout, font="MathJax_Main")
+
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    elif event == "-HERE-" :
+        webbrowser.open("https://www.makeuseof.com/tag/raspberry-pi-control-led/", new=1)
+    elif event == "-ON-" :
+        GPIO.output(ledPin, GPIO.HIGH)
+    elif event == "-OFF-" :
+        GPIO.output(ledPin, GPIO.LOW)
+        
+
+window.close()
